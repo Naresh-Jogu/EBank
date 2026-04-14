d47332afd32111355c947eb15af91c575f080f6c
Naresh Jogu <kumarn29346@gmail.com>
2026-04-14 08:09:00 +0000

    Implement Home UI and integrate Header component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,20 @@
 import Header from '../Header'
+import './index.css'
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <Header />
-      <h1>Home</h1>
+
+      <div className="home-content">
+        <h1 className="home-heading">Your Flexibility, Our Excellence</h1>
+
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/ebank-digital-card-img.png"
+          alt="digital card"
+          className="home-image"
+        />
+      </div>
     </div>
   )
 }
